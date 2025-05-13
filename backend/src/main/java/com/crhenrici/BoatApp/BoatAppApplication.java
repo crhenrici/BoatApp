3392962ddb08cdbce6dd12bab9c4be72ba54ab51
Cristian Henrici <henricri@students.zhaw.ch>
2025-05-13 14:26:35 +0200

    Initial commit

---
+++ backend/src/main/java/com/crhenrici/BoatApp/BoatAppApplication.java
@@ -0,0 +1,13 @@
+package com.crhenrici.BoatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatAppApplication.class, args);
+	}
+
+}
